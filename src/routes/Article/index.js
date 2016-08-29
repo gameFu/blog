2b7a087ae3daca27bcc3470896a06f9f421e0e96
gameFu <223328084@qq.com>
2016-08-30 00:47:12 +0800

    add article component

---
+++ src/routes/Article/index.js
@@ -0,0 +1,24 @@
+import { injectReducer } from '../../store/reducers'
+
+export default (store) => ({
+  path: 'article',
+  /*  Async getComponent is only invoked when route matches   */
+  getComponent (nextState, cb) {
+    /*  Webpack - use 'require.ensure' to create a split point
+        and embed an async module loader (jsonp) when bundling   */
+    require.ensure([], (require) => {
+      /*  Webpack - use require callback to define
+          dependencies for bundling   */
+      const Article = require('./containers/ArticleContainer').default
+      const reducer = require('./modules/article').default
+
+      /*  Add the reducer to the store on key 'counter'  */
+      injectReducer(store, { key: 'article', reducer })
+
+      /*  Return getComponent   */
+      cb(null, Article)
+
+    /* Webpack named bundle   */
+    }, 'counter')
+  }
+})
